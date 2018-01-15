2031ec464bb81ad0f53d79a90ad9bd6554a22b7d
Stewart Robertson <stewart_robertson@sky.com>
2018-01-15 10:59:52 +0000

    added watch capability

--- src/Clusterer/clusterer.ts
+++ src/Clusterer/clusterer.ts
@@ -1,14 +1,28 @@
 import * as errorMessages from "./errorMessages";
 
-export class Clusterer {
-
-    public clusterElements = <T extends {}>(elements: T[], k: number, distanceFn?: (t1: T, t2: T) => number) => {
-        const medoids = this.selectInitialMediods(elements, k);
+export class Clusterer<T> {
+    public static clusterElements = <T extends {}>(elements: T[], k: number, distanceFn?: (t1: T, t2: T) => number) => {
+        const instance = new Clusterer(elements, k, distanceFn);
         const ret: T[][] = new Array();
         return ret;
     }
 
-    private selectInitialMediods = <T extends {}>(elements: T[], k: number) => {
-        throw errorMessages.kGtElementArrLength;
+    private constructor(
+            public elements: T[],
+            public k: number,
+            public distanceFn?: (t1: T, t2: T) => number) {
+        const medoids = this.selectInitialMediods(elements, k);
+
+    }
+
+    private selectInitialMediods = (elements: T[], k: number) => {
+        if (k >= elements.length) {
+            throw errorMessages.kGtElementArrLength;
+        }
+        const ret: T[] = [];
+        for (let i = 0; i < k; i++) {
+            ret.push(elements[i]);
+        }
+        return ret;
     }
 }
---
+++ test/Clusterer/clusterer.spec.ts
@@ -0,0 +1,34 @@
+/* tslint:disable no-unused-expression*/
+
+import { assert, expect } from "chai";
+import "mocha";
+import { Clusterer } from "../../src/Clusterer/clusterer";
+import * as errorMessages from "../../src/Clusterer/errorMessages";
+
+describe("Clusterer", () => {
+
+    const sut = Clusterer;
+    it("a Clusterer can be instantiated", () => {
+        expect(sut).to.not.be.null;
+    });
+
+    const testCase = [[1, 0], [2, 1], [0, 3], [-1, 4]];
+    const testK = 2;
+
+    describe(`given elements: ${JSON.stringify(testCase)}, k: ${testK}`, () => {
+        describe(`with no explicit distance function`, () => {
+            it ("uses the euclidean distance function");
+            it ("returns a clustered array");
+            it (`contains ${testCase.length} elements in total`);
+        });
+    });
+
+    const testCase2 = [[1, 0], [2, 1], [0, 3], [-1, 4]];
+    const testK2 = 10;
+
+    describe(`given elements: ${JSON.stringify(testCase)}, k: ${testK}`, () => {
+        it ("Throws an error because k > elements.length", () => {
+            expect(() => sut.clusterElements (testCase2, testK2)).to.throw(errorMessages.kGtElementArrLength);
+        });
+    });
+});
